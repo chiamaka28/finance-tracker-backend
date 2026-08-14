6c76752ccd9b13d31c1780d6e32b7d88d61c0f6d
chiamaka28 <chiamakamaduike@gmail.com>
2026-08-14 11:48:19 +0100

    feat(security): implement global and auth route rate limiting

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, {type Express, type Response} from 'express';
+import { rateLimit } from 'express-rate-limit';
 import authRouter from './modules/auth/auth.router';
 import TransactionRouter from './modules/transaction/transaction.router';
 import budgetRouter from './modules/budget/budget.router';
@@ -10,12 +11,30 @@ import cors from 'cors';
 
 const app: Express = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again later.',
+  statusCode: 429,
+});
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 5, // Only 5 attempts allowed
+  message: 'Too many login attempts. Please try again later.',
+});
+
+
 
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Rate-limiting Middleware
+
+app.use(limiter);
+
+
 //Cors
 app.use(cors());
 //Authentication Middleware
@@ -24,7 +43,7 @@ app.use(cors());
 app.get('/', (req, res: Response) => {
   res.send('Hello, World!');
 });
-app.use('/api/auth', authRouter);
+app.use('/api/auth', authLimiter, authRouter);
 app.use('/api/overview', authenticate, OverviewRouter);
 app.use('/api/transactions', authenticate, TransactionRouter);
 app.use('/api/budget', authenticate,  budgetRouter);
